91b81aa11a1ea238f9294ab235c4421fbecaf3a4
jakareya76 <jakareya1306@gmail.com>
2024-02-12 20:47:49 +0600

    create rectangle calculator

--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -2,5 +2,13 @@ function calculateRectangleArea() {
   const rectangleWidthInput = document.getElementById("rectangle-width");
   const rectangleLengthInput = document.getElementById("rectangle-length");
 
-  //   console.log(rectangleWidthInput, rectangleLengthInput);
+  const rectangleWidthValue = rectangleWidthInput.value;
+  const rectangleLengthValue = rectangleLengthInput.value;
+
+  const width = parseFloat(rectangleWidthValue);
+  const length = parseFloat(rectangleLengthValue);
+
+  const area = width * length;
+
+  console.log(area);
 }
