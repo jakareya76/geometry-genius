e14d43dffbedaf088494ffbc039e483d94135dba
jakareya76 <jakareya1306@gmail.com>
2024-02-13 14:24:19 +0600

    create a common function to get input value

--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -1,14 +1,17 @@
 function calculateRectangleArea() {
-  const rectangleWidthInput = document.getElementById("rectangle-width");
-  const rectangleLengthInput = document.getElementById("rectangle-length");
-
-  const rectangleWidthValue = rectangleWidthInput.value;
-  const rectangleLengthValue = rectangleLengthInput.value;
-
-  const width = parseFloat(rectangleWidthValue);
-  const length = parseFloat(rectangleLengthValue);
+  const width = getInputValueById("rectangle-width");
+  const length = getInputValueById("rectangle-length");
 
   const area = width * length;
 
   console.log(area);
+}
+
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputValue = inputField.value;
+
+  const value = parseFloat(inputValue);
+
+  return value;
 }
