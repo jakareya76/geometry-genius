1ca6072beed6ac161ff2df28becb5daed41fba50
jakareya76 <jakareya1306@gmail.com>
2024-02-13 14:26:11 +0600

    refactor triangle.js for common input value function

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -1,14 +1,17 @@
 function calculateTriangleArea() {
-  const triangleBaseInput = document.getElementById("triangle-base");
-  const triangleHeightInput = document.getElementById("triangle-height");
-
-  const triangleBaseValue = triangleBaseInput.value;
-  const triangleHeightValue = triangleHeightInput.value;
-
-  const base = parseFloat(triangleBaseValue);
-  const height = parseFloat(triangleHeightValue);
+  const base = getInputValueById("triangle-base");
+  const height = getInputValueById("triangle-height");
 
   const area = 0.5 * base * height;
 
   console.log(area);
+}
+
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputValue = inputField.value;
+
+  const value = parseFloat(inputValue);
+
+  return value;
 }
